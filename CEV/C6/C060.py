32e34ba9881675cbdba5a3f0628b72607dc633fd
André Wanderley <154604167+Andrewanport@users.noreply.github.com>
2024-03-24 19:15:29 -0300

    Update C060.py

--- CEV/C6/C060.py
+++ CEV/C6/C060.py
@@ -5,4 +5,38 @@ Faça um programa que leia um número qualquer e mostre o seu fatorial.
 
 """
 
+from math import factorial
+
+# Input
+N = int(input('Digite um número para calcular seu fatorial: '))
+
+# Counter
+C = N
+
+# Main operation
+F = factorial(N)
+
+# Main print
+print('{}! = '.format(N), end='')
+
+# Loop
+while C > 0:
+    print('{}'.format(C), end='')
+    print(' x ' if C > 1 else ' = ', end='')
+
+    # Counter modify
+    C -= 1
+
+print(' {}'.format(F),end='')
+
+""" 
+
+Também poderiamos fazer sem utilizar a propriedade factorial 
+
+Bastaria iniciar a variável F em 1 
+
+e incrementar no loop como: F *= C
+
+"""
+
 
