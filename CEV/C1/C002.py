faa0cf075d929a6f2a1fd0502fb07a83cceb1c1f
André Wanderley <154604167+Andrewanport@users.noreply.github.com>
2024-03-30 15:29:16 -0300

    Update C002.py

--- CEV/C1/C002.py
+++ CEV/C1/C002.py
@@ -1,13 +1,6 @@
 # Exercício Python 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 #.
 
-N1 = int(input('Digite um número:'))
-N2 = int(input('Digite outro número:'))
+N = str(input('Digite o seu nome:'))
 
-S = N1 + N2
-
-# A forma menos otimizada seria:
-# print('A soma entre', N1, 'e', N2, 'vale', S)
-
-# A forma mais inteligente seria:
-print('A soma ente {} e {} vale {}'.format(N1, N2, S))
+print('Seja bem-vindo, {}'.format(N))
