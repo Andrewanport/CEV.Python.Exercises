09c6ecb88a998f21268928944fe6b23e97db2090
André Wanderley <154604167+Andrewanport@users.noreply.github.com>
2024-08-29 15:20:44 -0300

    Delete CEV/C3 directory

--- CEV/C3/C025.py
+++
@@ -1,5 +0,0 @@
-# Crie um programa que leia um nome e diga se tem 'Silva'
-# .
-
-nome = str(input('Qual o seu nome? ')).strip()
-print('Seu nome possui "SILVA"? {}'.format('SILVA' in nome.upper().split()))
